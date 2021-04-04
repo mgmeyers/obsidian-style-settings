82ac02af48de6b685a81586509e2dae1fedb9dad
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-04-04 14:53:42 -0700

    Pick up on all style config blocks

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { parse } from "yaml";
 import "@simonwep/pickr/dist/themes/nano.min.css";
 import "./pickerOverrides.css";
 
-const settingRegExp = /^\/\*\s*@settings[\r\n]+?([\s\S]+?)\*\//;
+const settingRegExp = /\/\*\s*@settings[\r\n]+?([\s\S]+?)\*\//g;
 
 export default class CSSSettingsPlugin extends Plugin {
   settingsManager: CSSSettingsManager;
@@ -34,16 +34,18 @@ export default class CSSSettingsPlugin extends Plugin {
         for (let i = 0, len = styleSheets.length; i < len; i++) {
           const sheet = styleSheets.item(i);
           const text = sheet.ownerNode.textContent.trim();
-          const match = text.match(settingRegExp);
+          let match = settingRegExp.exec(text);
 
           if (match && match.length) {
             try {
-              const str = match[1].trim();
-              const settings = parse(str);
-
-              if (settings.name && settings.id && settings.settings) {
-                settingsList.push(settings as ParsedCSSSettings);
-              }
+              do {
+                const str = match[1].trim();
+                const settings = parse(str);
+  
+                if (settings.name && settings.id && settings.settings) {
+                  settingsList.push(settings as ParsedCSSSettings);
+                }
+              } while((match = settingRegExp.exec(text)) !== null);
             } catch (e) {
               console.error("Error parsing style settings: ", e);
             }
