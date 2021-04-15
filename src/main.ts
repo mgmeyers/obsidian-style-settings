444005953c815dce063a1a19f9e74861e7900246
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-04-15 10:53:37 -0700

    Add empty and error messages

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { App, Plugin, PluginSettingTab } from "obsidian";
 import { CSSSettingsManager } from "./SettingsManager";
 import {
   CleanupFunction,
-  createHeading,
   createSettings,
   CSSSetting,
   ParsedCSSSettings,
 } from "./settingHandlers";
-import { parse } from "yaml";
+import yaml from "js-yaml";
+import detectIndent from "detect-indent";
 
 import "@simonwep/pickr/dist/themes/nano.min.css";
 import "./pickerOverrides.css";
 import "./settings.css";
 
 const settingRegExp = /\/\*\s*@settings[\r\n]+?([\s\S]+?)\*\//g;
+const nameRegExp = /^name:\s*(.+)$/m;
+
+type ErrorList = Array<{ name: string; error: string }>;
 
 export default class CSSSettingsPlugin extends Plugin {
   settingsManager: CSSSettingsManager;
@@ -32,29 +35,42 @@ export default class CSSSettingsPlugin extends Plugin {
       this.app.workspace.on("css-change", () => {
         const styleSheets = document.styleSheets;
         const settingsList: ParsedCSSSettings[] = [];
+        const errorList: ErrorList = [];
 
         for (let i = 0, len = styleSheets.length; i < len; i++) {
           const sheet = styleSheets.item(i);
           const text = sheet.ownerNode.textContent.trim();
+
           let match = settingRegExp.exec(text);
 
           if (match && match.length) {
+            const nameMatch = text.match(nameRegExp);
+            const name: string | null = nameMatch ? nameMatch[1] : null;
+
             try {
               do {
-                const str = match[1].trim().replace(/\t/g, "  ");
-                const settings = parse(str);
+                const str = match[1].trim();
+                const indent = detectIndent(str);
+                const settings = yaml.load(str.replace(/\t/g, indent.indent), {
+                  filename: name,
+                }) as ParsedCSSSettings;
 
-                if (settings.name && settings.id && settings.settings) {
-                  settingsList.push(settings as ParsedCSSSettings);
+                if (
+                  typeof settings === "object" &&
+                  settings.name &&
+                  settings.id &&
+                  settings.settings
+                ) {
+                  settingsList.push(settings);
                 }
               } while ((match = settingRegExp.exec(text)) !== null);
             } catch (e) {
-              console.error("Error parsing style settings: ", e);
+              errorList.push({ name, error: `${e}` });
             }
           }
         }
 
-        this.settingsTab.setSettings(settingsList);
+        this.settingsTab.setSettings(settingsList, errorList);
         this.settingsManager.initClasses();
       })
     );
@@ -75,6 +91,7 @@ class CSSSettingsTab extends PluginSettingTab {
   plugin: CSSSettingsPlugin;
   cleanupFns: CleanupFunction[] = [];
   settings: ParsedCSSSettings[] = [];
+  errorList: ErrorList = [];
 
   constructor(app: App, plugin: CSSSettingsPlugin) {
     super(app, plugin);
@@ -89,8 +106,10 @@ class CSSSettingsTab extends PluginSettingTab {
     this.cleanupFns.forEach((fn) => fn && fn());
   }
 
-  setSettings(settings: ParsedCSSSettings[]) {
+  setSettings(settings: ParsedCSSSettings[], errorList: ErrorList) {
     this.settings = settings;
+    this.errorList = errorList;
+
     this.plugin.settingsManager.setConfig(settings);
 
     if (this.containerEl.parentNode) {
@@ -98,11 +117,54 @@ class CSSSettingsTab extends PluginSettingTab {
     }
   }
 
+  displayErrors() {
+    let { containerEl, errorList } = this;
+
+    errorList.forEach((err) => {
+      containerEl.createDiv({ cls: "style-settings-error" }, (wrapper) => {
+        wrapper.createDiv({ cls: "style-settings-error-name", text: `Error: ${err.name}` });
+        wrapper.createDiv({
+          cls: "style-settings-error-desc",
+          text: err.error,
+        });
+      });
+    });
+  }
+
+  displayEmpty() {
+    let { containerEl } = this;
+
+    containerEl.createDiv({ cls: "style-settings-empty" }, (wrapper) => {
+      wrapper.createDiv({
+        cls: "style-settings-empty-name",
+        text: "No style settings found",
+      });
+      wrapper.createDiv({ cls: "style-settings-empty-desc" }).appendChild(
+        createFragment((frag) => {
+          frag.appendText(
+            "Style settings configured by theme and plugin authors will show up here. You can also create your own configuration by creating a CSS snippet in your vault. "
+          );
+          frag.createEl("a", {
+            text: "Click here for details and examples.",
+            href:
+              "https://github.com/mgmeyers/obsidian-style-settings#obsidian-style-settings-plugin",
+          });
+        })
+      );
+    });
+  }
+
   generate(settings: ParsedCSSSettings[]) {
     let { containerEl, plugin } = this;
 
     containerEl.empty();
+
     this.cleanup();
+    this.displayErrors();
+
+    if (settings.length === 0) {
+      return this.displayEmpty();
+    }
 
     const cleanupFns: CleanupFunction[] = [];
 
@@ -116,7 +178,7 @@ class CSSSettingsTab extends PluginSettingTab {
           collapsed: true,
           resetFn: () => {
             plugin.settingsManager.clearSection(s.id);
-            this.generate(this.settings)
+            this.generate(this.settings);
           },
         },
         ...s.settings,
