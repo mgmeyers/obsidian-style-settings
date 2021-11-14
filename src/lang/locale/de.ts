5711018a7f7f587517f8e8e51cee7f3a26845f81
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-11-14 15:08:21 -0800

    Build DE translation

--- src/lang/locale/de.ts
+++ src/lang/locale/de.ts
@@ -1,8 +1,11 @@
 export const de = {
   "Default:": "Standard:",
   "Error:": "Fehler:",
-  "missing default light value, or value is not in a valid color format": "Fehlender heller standard Wert oder Wert ist in keinem validen Farb-Format",
-  "missing default dark value, or value is not in a valid color format": "Fehlender dunkler standard Wert oder Wert ist in keinem validen Farb-Format",
-  "missing default value, or value is not in a valid color format": "Fehlender standard Wert oder Wert ist in keinem validen Farb-Format",
+  "missing default light value, or value is not in a valid color format":
+    "Fehlender heller standard Wert oder Wert ist in keinem validen Farb-Format",
+  "missing default dark value, or value is not in a valid color format":
+    "Fehlender dunkler standard Wert oder Wert ist in keinem validen Farb-Format",
+  "missing default value, or value is not in a valid color format":
+    "Fehlender standard Wert oder Wert ist in keinem validen Farb-Format",
   "missing default value": "Fehlender standard Wert",
-}
+};
